3b19ce6733936aa951d8e3527da1944628149750
Jeff Registre <jeffreyregistre@gmail.com>
2022-06-28 22:16:10 -0400

    wasm-pack and svelte experiment

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import {hello_wasm} from "wasm-greet"
+
+	export let name;
+
+	let wasmGreet = hello_wasm();
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<h2>Here is hello from wasm: {wasmGreet}</h2>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
